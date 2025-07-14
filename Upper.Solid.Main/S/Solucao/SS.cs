a8813bb117b1015ced03de33449a6d7c83474429
IvanSSantana <ivansantospriv@gmail.com>
2025-07-13 23:30:34 -0300

    commit 1 - Concluindo SRP

---
+++ Upper.Solid.Main/S/Solucao/SS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upper.Solid.S.Solucao
+{
+    public class Garcom
+    {
+        public void AnotarPedidos() { }
+    }
+
+    public class Gerente
+    {
+        public void Administrar() { }
+    }
+
+    public class Faxineiro
+    {
+        public void Limpar() { }
+    }
+
+    // Nota-se correto, pois cada classe tem sua individual função
+}
